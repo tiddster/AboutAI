d053a47b32778daa9aa3020133184d1d5b18b60e
tiddster <864187149@qq.com>
2022-04-02 22:43:09 +0800

    优化梯度下降函数

--- MachineLearning/Regression/LinearRegressionModel.py
+++ MachineLearning/Regression/LinearRegressionModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class LRM():
-    def __init__(self, theta0 = 0, theta1 = 0):
+    def __init__(self, theta0=0, theta1=0):
         self.theta0 = theta0
         self.theta1 = theta1
 
@@ -15,7 +15,7 @@ class LRM():
             total += (self.h(x)-y)**2
         return 1/(2*m) * total
     
-    def GradientDescent(self,xs, ys, alpha = 1, k=10e-12):
+    def GradientDescent(self, xs, ys, alpha=0.1, k=10e-6):
         m = len(xs)
         total0 = 0
         total1 = 0
@@ -26,7 +26,8 @@ class LRM():
         lastTheta1 = self.theta1
         self.theta0 -= alpha * total0 / m
         self.theta1 -= alpha * total1 / m
-        if abs(lastTheta0 - self.theta0) < k and abs(lastTheta1 - self.theta1):
+        print(self.theta0 , self.theta1)
+        if abs(lastTheta0 - self.theta0) < k and abs(lastTheta1 - self.theta1) < k:
             return self.theta0, self.theta1
         else:
             return self.GradientDescent(xs, ys, alpha)
@@ -34,6 +35,6 @@ class LRM():
 if __name__ == '__main__':
     xs = [5,3,0,4]
     y = [4,4,1,3]
-    lrm = LRM(0,1)
+    lrm = LRM(0,0)
     print(lrm.J(xs, y))
     print(lrm.GradientDescent(xs, y))
