635a50ea4e6c41748063f98c5fa75a1fd23e30cd
tiddster <864187149@qq.com>
2022-04-06 22:48:16 +0800

    矩阵算法实现

--- MachineLearning/Regression/MultivariateLinearRegression.py
+++ MachineLearning/Regression/MultivariateLinearRegression.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 # 多变量的线性回归函数
 # xs 是二维, x 是一维
 class MLR():
@@ -13,9 +15,8 @@ class MLR():
         # 扩充一列x0 = 0， 便于与theta数组做矩阵内积计算
         Ones = np.ones((self.length, 1))
         self.paddedXs = np.hstack((Ones, self.paddedXs))
-        print(self.paddedXs)
 
-    def h(self,x):
+    def h(self, x):
         return np.dot(self.theta, x)
 
     def J(self):
@@ -23,7 +24,12 @@ class MLR():
         total = 0
         for x, y in zip(self.paddedXs, self.ys):
             total += (self.h(x) - y) ** 2
-        return total / (2*m)
+        return total / (2 * m)
+
+    def JInMatrix(self):
+        m = self.length
+        res = np.sum((np.dot(self.theta, self.paddedXs.T) - self.ys) ** 2)
+        return res / (2 * m)
 
     def GradientDescent(self, alpha=0.1, k=10e-6):
         lastCost = self.J()
@@ -33,9 +39,11 @@ class MLR():
         tempThetas = self.theta.copy()
 
         for i in range(len(self.theta)):
-            for x, y in zip(self.paddedXs,self.ys):
+            for x, y in zip(self.paddedXs, self.ys):
                 total += (self.h(x) - y) * x[i]
             tempThetas[i] -= alpha * total / m
+
+        print(tempThetas)
 
         self.theta = tempThetas.copy()
         nowCost = self.J()
@@ -45,15 +53,31 @@ class MLR():
         else:
             return self.GradientDescent(alpha=alpha)
 
+    def GradientDescentInMatrix(self, alpha=0.1, k=10e-6):
+        lastCost = self.JInMatrix()
+
+        m = self.length
+        tempThetas = self.theta - alpha / m * np.dot((np.dot(self.theta,self.paddedXs.T) - self.ys ), self.paddedXs)
+
+        self.theta = tempThetas
+        nowCost = self.JInMatrix()
+
+        if abs(nowCost - lastCost) < k:
+            return self.theta
+        else:
+            return self.GradientDescentInMatrix(alpha=alpha)
+
 if __name__ == '__main__':
-    thetas = [0,1]
+    thetas = [0, 1]
     xs = [
         [5],
         [3],
         [0],
         [4]
     ]
-    ys = [4,4,1,3]
-    mlr = MLR(thetas,xs,ys)
+    ys = [4, 4, 1, 3]
+    mlr = MLR(thetas, xs, ys)
     print(mlr.J())
-    print(mlr.GradientDescent())
+    print(mlr.JInMatrix())
+    #print(mlr.GradientDescent())
+    print(mlr.GradientDescentInMatrix())
--- MachineLearning/Regression/NormalEquation.py
+++ MachineLearning/Regression/NormalEquation.py
@@ -1,18 +1,19 @@
 import numpy as np
 
+
 class NE():
     def __init__(self, xs, ys):
         self.xs = xs
         self.ys = ys
         self.length = len(ys)
 
-        ones = np.ones([self.length,1])
-        self.X = np.hstack((ones,self.xs))
+        ones = np.ones([self.length, 1])
+        self.X = np.hstack((ones, self.xs))
 
     def NormalEquation(self):
         X = self.X
-        print(X,X.T)
-        print(np.outer(X.T,X))
+        return np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), self.ys)
+
 
 if __name__ == '__main__':
     xs = [
@@ -22,5 +23,5 @@ if __name__ == '__main__':
         [4]
     ]
     ys = [4, 4, 1, 3]
-    ne = NE(xs,ys)
+    ne = NE(xs, ys)
     ne.NormalEquation()
