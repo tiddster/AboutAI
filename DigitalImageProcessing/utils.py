766aaf8b379780188cbd5a7403367cbf4ab635a2
tiddster <864187149@qq.com>
2022-05-06 10:19:14 +0800

    bmp2jpg

---
+++ DigitalImageProcessing/utils.py
@@ -0,0 +1,2 @@
+biu_path = 'C:\\Users\\tiddler\\Desktop\\Python\\AI\\DigitalImageProcessing\\dataset\\biu.jpg'
+lena_path = 'C:\\Users\\tiddler\\Desktop\\Python\\AI\\DigitalImageProcessing\\dataset\\lena.bmp'
