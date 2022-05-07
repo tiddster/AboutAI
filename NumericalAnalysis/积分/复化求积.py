4d0df4e59f5fda3dc281fecbe9e92a0ec082e911
tiddster <864187149@qq.com>
2022-05-07 11:29:54 +0800

    数值分析

--- NumericalAnalysis/积分/复化求积.py
+++ NumericalAnalysis/积分/复化求积.py
@@ -1,36 +1,31 @@
 import math
 
 
-def complex_quad(a,b,n,f):
-    h = (b-a)/n
+def complex_quad(a, b, n, f):
+    h = (b - a) / n
     x = a
     S = 0
     for i in range(n):
         x += h
         S += f(x)
-    return (h/2) * (2*S + f(a) + f(b)), h
+    return (h / 2) * (2 * S + f(a) + f(b)), h
 
-def simpson_quad(a,b,n,f):
-    h = (b-a)/n
+
+def simpson_quad(a, b, n, f):
+    h = (b - a) / n
     x = a
     S = 0
     for i in range(n):
-        x1 = x + i*h
-        x2 = x + (2*i - h)/2*h
-        S += 4*f(x1)
-        S += 2*f(x2)
-    return (S + f(a) + f(b)) * h/6,h
+        x1 = x + i * h
+        x2 = x + (2 * i - h) / 2 * h
+        S += 4 * f(x1)
+        S += 2 * f(x2)
+    return (S + f(a) + f(b)) * h / 6, h
 
 
-f = lambda x: math.e ** x
-i = 0
+f = lambda x: math.e ** (-x ** 2)
 k = 0.00005
-while True:
-    i += 1
-    c,h = complex_quad(0,1,i,f)
-    ori = f(1)-f(0)
-    print(f"复化求积公式结果{c},步长为{h},误差为{abs(c - ori)}")
-    if abs(c - ori) < k:
-        print(f"当步长为{h}时,误差小于0.5*10e-4")
-        break
 
+c, h = complex_quad(0, 1, 8, f)
+ori = f(1) - f(0)
+print(f"复化求积公式结果{c},步长为{h},误差为{abs(c - ori)}")
