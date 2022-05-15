e552bad3de230e63ff4611d004c3051956bf81ba
tiddster <864187149@qq.com>
2022-05-15 17:57:44 +0800

    bmp与pic互转

--- DigitalImageProcessing/bmp与jpg/bmp_struct.py
+++ DigitalImageProcessing/bmp与jpg/bmp_struct.py
@@ -13,19 +13,19 @@ class BMP:
     def __init__(self, file_path):
         tp = self.load_bmp_header(file_path)
 
-        self.tag = tp[0]
-        self.fileSize = tp[1]
-        self.A = tp[2]
-        self.B = tp[3]
-        self.rgbOffset = tp[4]
-        self.infoSize = tp[5]
-        self.width = tp[6]
-        self.height = tp[7]
-        self.pane = tp[8]
-        self.color = tp[9]
-        self.compress = tp[10]
-        self.rgbSize = tp[11]
-        self.C, self.D, self.E, self.F = tp[12:16]
+        self.tag = tp[0]                    #图像标注
+        self.fileSize = tp[1]               #bmp文件大小
+        self.A = tp[2]                      #保留字段
+        self.B = tp[3]                      #保留字段
+        self.rgbOffset = tp[4]              #rgb信息偏移量，也就是rgb信息开始的地方
+        self.infoSize = tp[5]               #DIB header大小
+        self.width = tp[6]                  #图像宽度
+        self.height = tp[7]                 #图像高度
+        self.pane = tp[8]                   #色彩平面的数量 只能为1
+        self.color = tp[9]                  #图像色彩位数，4/8/24/36
+        self.compress = tp[10]              #图像压缩方式
+        self.rgbSize = tp[11]               #图像的大小，不算头文件
+        self.rowResolution, self.colResolution, self.E, self.F = tp[12:16]          #分别是纵向分辨率，横向分辨率，色彩数量，重要色彩数量，在此程序中用不到
 
         self.image = self.load_bmp_rgb(file_path)
 
@@ -47,6 +47,7 @@ class BMP:
         self.rgbOffset = 54
         self.width = pic.col
         self.height = pic.row
+        self.pane = 1
         self.rgbSize = self.width * self.height * 3
         self.fileSize = self.rgbSize + self.rgbOffset
 
@@ -56,7 +57,7 @@ class BMP:
     def save_bmp(self, file_path):
         f = open(file_path, 'wb')
         data = pack('<2sI2H4I2H6I', self.tag, self.fileSize, self.A, self.B, self.rgbOffset, self.infoSize,
-                    self.width, self.height, self.pane, self.color, self.compress, self.rgbSize, self.C, self.D, self.E, self.F)
+                    self.width, self.height, self.pane, self.color, self.compress, self.rgbSize, self.rowResolution, self.colResolution, self.E, self.F)
         f.write(data)
         rgb = self.image.ravel()
         for c in rgb:
