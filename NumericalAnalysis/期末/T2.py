f6fbd7145bea1fa9a5b5f1d3774315f197c03827
tiddster <864187149@qq.com>
2022-06-01 11:30:08 +0800

    期末

--- NumericalAnalysis/期末/T2.py
+++ NumericalAnalysis/期末/T2.py
@@ -1,29 +1,49 @@
+import math
 import random
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 # 已知条件：求积节点x0、x1、……、xn，对应函数值y0、y1、……、yn，积分上下限a、b；
-# 生成11个x，随机生成11个y，构造一个10次的拉格朗日函数
-a, b = 0, 10
-xs = np.linspace(0, 10, 11)
-ys = [random.uniform(5, 10) for i in range(11)]
+# 生成11个x，构造一个10次的拉格朗日函数
+from scipy import integrate
 
 
-def lagrange(x):
+def f(x):
+    return math.sin(x) * math.cos(x)
+a, b = 0, 1
+xs = np.linspace(0, 1, 11)
+ys = [f(x) for x in xs]
+
+
+def lagrange(a, b):
     Lx = 0
-    for i in range(11):
-        li = 1
-        for j in range(11):
-            if i != j:
-                li *= (x - xs[j]) / (xs[i] - xs[j])
-        Lx += li * ys[i]
+    for i in range(len(xs)):
+        Ai = integral(a, b, i)  # 四舍五入
+        Lx += Ai * ys[i]
     return Lx
 
-x_pred, y_pred = np.linspace(0,10,100), []
-for x in x_pred:
-    y_pred.append(lagrange(x))
 
-plt.plot(xs, ys, '.')
-plt.plot(x_pred, y_pred)
-plt.show()
+def lagrangeB(x, i):
+    li = 1
+    for j in range(len(xs)):
+        if i != j:
+            li *= (x - xs[j]) / (xs[i] - xs[j])
+    return li
+
+
+def integral(a, b, k):
+    N, h, S = 1000, (b - a) / 1000, 0
+    for i in range(N):
+        x = a + h * i
+        S += h * lagrangeB(x, k)
+    return S
+
+
+def quad(a, b):
+    res = integrate.quad(f, a, b)[0]
+    return res
+
+
+print(f"利用高斯求积公式的结果{quad(0,1)}")
+print(f"利用拉格朗日型插值求积分结果{lagrange(0, 1)}")
